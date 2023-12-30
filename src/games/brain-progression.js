1b94e8a6b92f44d29d2cdf9ca2a0f51e5ae22d4b
Pavel Kochetkov <kochetkov_p@mail.ru>
2023-12-31 00:35:45 +0300

    Implementation of the logic of the game "Arithmetic Progression"

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,4 +1,34 @@
 #!/usr/bin/env node
+import { getRandomNumber, getRandomIndex } from "../utilities.js";
+import runGameRound from "../index.js";
+
+const description = 'What number is missing in the progression?';
+const minNumber = 5;
+const maxNumber = 9;
+
+const getProgression = (start, step, length) => {
+    const progressionArray = [];
+    for (let i = 0; i <= length; i += 1) {
+        progressionArray.push(start + i * step);
+    }
+
+    return progressionArray;
+};
+
+const generateGameRound = () => {
+    const start = getRandomNumber(minNumber, maxNumber);
+    const step = getRandomNumber(minNumber, maxNumber);
+    const length = getRandomNumber(minNumber, maxNumber);
+    const progression = getProgression(start, step, length);
+    const index = getRandomIndex(progression);
+    const searchElement = progression[index];
+    progression[index] = '..';
+    const question = `${progression.join(' ')}`;
+    const correctAnswer = searchElement.toString();
+    
+    return [question, correctAnswer];
+};
+
 export default () => {
-    console.log('progression')
-}
+    runGameRound(description, generateGameRound);
+};
