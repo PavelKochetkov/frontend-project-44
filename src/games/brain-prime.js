12800d3701f488e40894fa3860642f88d17c57fb
Pavel Kochetkov <kochetkov_p@mail.ru>
2023-12-31 20:38:38 +0300

    implementation of the logic of the game "Prime number"

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,4 +1,29 @@
 #!/usr/bin/env node
+import { getRandomNumber } from "../utilities.js";
+import runGameRound from "../index.js";
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const minNumber = 1;
+const maxNumber = 100;
+
+const isPrime = (number) => {
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            return false;
+        };
+    }
+    
+    return true;
+};
+
+const generateGameRound = () => {
+    const number = getRandomNumber(minNumber, maxNumber);
+    const question = `${number}`;
+    const correctAnswer = isPrime(number) ? 'yes' : 'no';
+
+    return [question, correctAnswer];
+};
+
 export default () => {
-    console.log('prime');
+    runGameRound(description, generateGameRound);
 };
